41e5eeeb178cac006d6fd38ce4b7c2f1a86f6570
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-10-17 20:03:25 +0200

    Add project files.

---
+++ Algebra.WebShop.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Algebra.WebShop.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
