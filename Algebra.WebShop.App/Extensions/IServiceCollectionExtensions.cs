39bafbbf6fb1e8967fa3bddcaed65d91b59f890d
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-11-07 18:57:44 +0100

    extensions

---
+++ Algebra.WebShop.App/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Algebra.WebShop.App.Data;
+using Algebra.WebShop.App.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Algebra.WebShop.App.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Add services to the container.
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddDefaultIdentity<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+        })
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("RequireAdminRole", policy => policy
+                .RequireAuthenticatedUser()
+                .RequireRole("Admin"));
+        });
+
+        services.AddControllersWithViews();
+
+        return services;
+    }
+}
--- Algebra.WebShop.App/Program.cs
+++ Algebra.WebShop.App/Program.cs
@@ -1,7 +1,4 @@
-using Algebra.WebShop.App.Data;
-using Algebra.WebShop.App.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using Algebra.WebShop.App.Extensions;
 
 namespace Algebra.WebShop.App;
 
@@ -11,62 +8,11 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
-            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
-
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddDefaultIdentity<ApplicationUser>(options => 
-        { 
-            options.SignIn.RequireConfirmedAccount = false; 
-        })
-        .AddRoles<IdentityRole>()
-        .AddEntityFrameworkStores<ApplicationDbContext>();
-
-        builder.Services.AddAuthorization(options =>
-        {
-            options.AddPolicy("RequireAdminRole", policy => policy
-                .RequireAuthenticatedUser()
-                .RequireRole("Admin"));
-        });
-
-        builder.Services.AddControllersWithViews();
+        builder.Services.ConfigureServices(builder.Configuration);
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "Admin",
-            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.MapRazorPages();
+        app.Configure();
 
         app.Run();
     }
