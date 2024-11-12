6fb4b1cd16da417b60fe7799cb8d43e2fcdf050a
Ratko Ćosić <ratko_cosic@hotmail.com>
2024-11-12 20:20:09 +0100

    init

---
+++ Algebra.WebShop.App/Views/Shared/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using Algebra.WebShop.App.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algebra.WebShop.App.Views.Shared;
+
+//[ViewComponent(Name = "ShoppingCart")]
+public class ShoppingCart : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        return View("Default", cart);
+    }
+}
